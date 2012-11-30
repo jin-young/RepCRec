58b28db176fbc663327bfa25d219ad33819083ba
ss6321 <ss6321@nyu.edu>
2012-11-30 17:24:37 -0500

    ge

--- src/adb_tm.erl
+++ src/adb_tm.erl
@@ -1,37 +1,36 @@
 -module(adb_tm).
 %-export([start/0, stop/0, beginT/1, endT/1, write/3, read/2, beginRT/1, dump/0, dump/1, fail/1, recover/1).
--export([start/0, beginT/1, endT/1, write/3, read/2, beginRO/1, dump/0, dump/1, fail/1, recover/1]).
+-export([start/0, beginT/1, endT/1, w/3, r/2, beginRO/1, dump/0, dump/1, fail/1, recover/1]).
 
 start() ->
      register(adb_tm, spawn(fun() ->
 				     loop() end)).
 beginT(Tid) ->
 	rpc({beginT, Tid}).
-    %io:fwrite("beginT ~s~n", [Tid]).
-	%rpc:call(client@localhost, adb_tm, beginT, [Tid]).
+		
 beginRO(Tid) ->
-	rpc({beginT, Tid}).
+	rpc({beginRO, Tid}).
 
 dump() ->
-	io:fwrite("dump ~n").
+    rpc({dump}).
 	
 dump(Tid) ->
-	io:fwrite("dump ~s~n", [Tid]).
+    rpc({dump,Tid}).
 	
 fail(Sid) ->
-	io:fwrite("fail ~s~n", [Sid]).
+    rpc({fail,Sid}).
 
 recover(Sid) ->
-	io:fwrite("recover ~s~n", [Sid]).
+	rpc({recover,Sid}).
 	
-read(Tid, ValId) ->
-	io:fwrite("read ~s~n", [Tid]).
+r(Tid, ValId) ->
+	rpc({r,{Tid,ValId}}).
 
-write(Tid, ValId, Value) ->
+w(Tid, ValId, Value) ->
 	rpc({w, {Tid, ValId, Value}}).
 
 endT(Tid) ->
-	io:fwrite("end ~s~n", [Tid]).
+	rpc({endT,Tid}).
 	
 registerT(Tid) ->
     put(Tid, {ok}).
@@ -54,28 +53,28 @@ loop() ->
 	    %if lookupT(TransId) == undefined ->
 		    %NewTransId = spawn(fun() -> adb_tran:start()), registerT(newTransId), From ! {ok};
 	    loop();
-	{From, {endT, TransId}} ->
-		From ! {adb_tm, TransId},
+	{From, {endT, Tid}} ->
+		From ! {adb_tm, Tid},
 	    loop();
-	{From, {w, {TransId, Variable, Value}}} ->
-		From ! {adb_tm, {TransId, Variable, Value}},
+	{From, {w, {Tid, ValId, Value}}} ->
+		From ! {adb_tm, {Tid, ValId, Value}},
 	    loop();
-	{From, {read, {TransId, Variable}}} ->
-		From ! {adb_tm, {TransId, Variable}},
+	{From, {r, {Tid, ValId}}} ->
+		From ! {adb_tm, {Tid, ValId}},
 	    loop();
-	{From, {beginRO, TransId}} ->
-		From ! {adb_tm, TransId},
+	{From, {beginRO, Tid}} ->
+		From ! {adb_tm, Tid},
 	    loop();
 	{From, {dump}} ->
-		From ! {adb_tm, "dumpppp"},
+		From ! {adb_tm, dump},
 	    loop();
-	{From, {dump, SiteId}} ->
-		From ! {adb_tm, SiteId},
+	{From, {dump, Sid}} ->
+		From ! {adb_tm, Sid},
 	    loop();
-    {From, {fail, SiteId}} ->
-		From ! {adb_tm, SiteId},
+    {From, {fail, Sid}} ->
+		From ! {adb_tm, Sid},
 	    loop();
-	{From, {recover, SiteId}} ->
-		From ! {adb_tm, SiteId},
+	{From, {recover, Sid}} ->
+		From ! {adb_tm, Sid},
 	    loop()
     end.
