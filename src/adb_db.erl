706be3232ff641ad4449c8fcbe62dc23acebcbf6
JinYoung Heo <classpath@gmail.com>
2012-11-30 18:20:01 -0500

    Intermediate DB implementation

--- src/adb_db.erl
+++ src/adb_db.erl
@@ -1,32 +1,68 @@
 -module(adb_db).
 
--export([start/0, stop/0, dump/0, fail/0, recover/0]).
+-export([start/0, stop/0, snapshot/0, snapshot/1, fail/1, recover/1, loop/0]).
 
 start() ->
-	ok.
+	ok = createServer(10, 1).
 
 stop() ->
     ok.
 
 %%--------------------------------------------------------------------
-%% Function: dump() -> {ok, SiteId, Variables}
+%% Function: snapshot(SiteId) -> {ok, SiteId, Variables}
 %%--------------------------------------------------------------------
-dump() ->
-    {ok, 1, []}.
-
+snapshot(SiteIdx) ->
+    rpc({snapshot, SiteIdx}).
+    
+%%--------------------------------------------------------------------
+%% Function: snapshot() -> {ok, {...}}
+%%--------------------------------------------------------------------
+snapshot() ->
+    {ok, {}}.    
+	
 %%--------------------------------------------------------------------
 %% Function: fail() -> {ok, Time}
 %%--------------------------------------------------------------------    
-fail() ->
-    {ok, time()}.
-
+fail(SiteIdx) ->
+		getId(SiteIdx) ! {self, {fail, SiteIdx}}.
+    
 %%--------------------------------------------------------------------
 %% Function: recover() -> {ok, Time}
 %%--------------------------------------------------------------------    
-recover() ->
-    {ok, time()}.
+recover(SiteIdx) ->
+   rpc({fail, SiteIdx}).
+   
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-
+rpc(Q) ->
+    adb_db1 ! {self(), Q},
+    receive
+	{adb_db1, Reply} ->
+	    Reply
+    end.
+    
+getId(SiteIdx) ->
+	list_to_atom(string:concat("adb_db", integer_to_list(SiteIdx))).
+	
+createServer(TotalServer, SiteIdx) ->
+	case SiteIdx =< TotalServer of
+		true -> io:format("create site ~p.~n", [getId(SiteIdx)]),
+				register(getId(SiteIdx), spawn(fun() -> loop() end)),
+			    createServer(TotalServer, SiteIdx+1);
+		false -> ok
+    end.
+    
+loop() ->
+	receive
+		{From, {snapshot, SiteIdx}} ->
+			io:format("Snapshot: ~p~n", [SiteIdx]),
+			loop();
+		{From, {fail, SiteIdx}} ->
+			io:format("Fail: ~p~n", [SiteIdx]),
+			loop();		
+		{From, {recover, SiteIdx}} ->
+			io:format("Recover: ~p~n", [SiteIdx]),
+			loop()			
+	end.
